7f96a05c538673b50200ae17b846af15bff15c91
robloxxa <robloxxa@yandex.ru>
2021-02-17 21:54:04 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+client.on('ready', () =>{
+    console.log('Manfredo ready');
+    console.log();
+})
+
+function findManfredo(message) {
+    let manfredo = [];
+    message.guild.emojis.cache.forEach(emojis => {
+        if (emojis.name.toLowerCase().includes('manfredo')){
+            manfredo.push(emojis)
+        }
+    })
+    return manfredo;
+}
+client.on('message', message => {
+    let manfredos = findManfredo(message);
+    console.log(message.content);
+    if (message.author.bot) return;
+    if(manfredos != []) {
+        if (message.content.includes(`@!${config.BOT_CLIENT}`)) {
+            message.channel.send(`<:${manfredos[0].name}:${manfredos[0].id}>`)
+        }
+        if (message.content.toLowerCase().includes('manfredo') || message.content.toLowerCase().includes(':Manfredo:')) {
+            // for (let i in manfredos) {
+            //     message.react(message.guild.emojis.cache.get(manfredos[i].id));
+            // }
+            message.react(message.guild.emojis.cache.get(manfredos[0].id));
+        }
+    }
+})
+client.login(config.BOT_TOKEN);
